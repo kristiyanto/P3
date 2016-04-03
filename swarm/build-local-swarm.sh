e13c0ec9840cc9bc84dc9936a6bca6b204065b19
Daniel Kristiyanto <danielkr@uw.edu>
2016-04-03 13:29:17 -0700

    swarm

--- swarm/build-local-swarm.sh
+++ swarm/build-local-swarm.sh
@@ -1,9 +1,17 @@
+
+# List exisiting docker engines
 docker-machine ls
+# Dowload and create swarm 
 docker pull swarm
 sid=$(docker run swarm create)
+# Create the swarm master
 docker-machine create -d virtualbox --swarm --swarm-master --swarm-discovery token://$sid swarm-master
+# Create the nodes. Repeat accordingly.
 docker-machine create -d virtualbox --swarm --swarm-discovery token://$sid swarm-node-01
 docker-machine create -d virtualbox --swarm --swarm-discovery token://$sid swarm-node-02
-
+# List of exsisting swarm
+docker run swarm list token://$sid
+# Get into the swarm environtment
 `docker-machine env --swarm swarm-master`
 docker ps
+
