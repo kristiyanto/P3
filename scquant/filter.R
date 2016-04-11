f791c5f54a08b60809f190cfae5ed44c3d1fabf5
Daniel Kristiyanto <danielkr@uw.edu>
2016-04-10 21:39:25 -0700

    scquant sanity check

--- scquant/filter.R
+++ scquant/filter.R
@@ -17,7 +17,7 @@ if (length(args)!=4) {
 }
 
 print("score.treshold:", score.treshold)
-print("error.treshold", error.treshold)
+print("error.treshold:", error.treshold)
 print("fdr", fdr)
 print("iteration", iteration)
 
@@ -29,12 +29,12 @@ mz.files <- list.files(path = ".", pattern ="mzid", all.files = F,
 ####################################### FILTERING ###################################################
 
 prj               <- MSnID(".")
-prj               <- read_mzIDs(object = prj, mzids = mz.files)
-prj               <- assess_termini(prj)
-prj               <- assess_missed_cleavages(prj)
-show(prj)
+mzid              <- read_mzIDs(object = prj, mzids = mz.files)
+mzid              <- assess_termini(mzid)
+mzid              <- assess_missed_cleavages(mzid)
+show(mzid)
 #print(paste("Before Filtering:",length(accessions(prj))))
-prj               <- apply_filter(prj, "numIrregCleavages == 0")
+prj               <- apply_filter(mzid, "numIrregCleavages == 0")
 prj               <- apply_filter(prj, "numMissCleavages < 2")
 prj$msmsScore     <- -log10(prj$`MS-GF:SpecEValue`)
 prj               <- correct_peak_selection(prj)
@@ -56,10 +56,9 @@ prj <- apply_filter(prj, "!grepl('XXX_', accession)")
 ####################################### QUANTIFICATION ###################################################
 msnset  <- as(prj, "MSnSet")
 msnset  <- combineFeatures(msnset, fData(msnset)$accession, redundancy.handler="unique", fun="sum",cv=FALSE)
-rm(prj)
-gc()
-tmp     <- exprs(msnset)
-tmp     <- cbind(Protein=row.names(tmp), as.data.frame(tmp))
+exprs.table     <- exprs(msnset)
+exprs.table     <- cbind(Protein=row.names(exprs.table), as.data.frame(exprs.table))
           
 write.table(tmp, file="SpectrumCount.txt", sep="\t", row.names = F, col.names = T)
 save(msnset, file = "msnset.rda")
+save.image(file="scquant_results.RData")
